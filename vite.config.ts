e0816203d4fe3c3f7220248159a793ffd9611544
MelnikovBogdan <maestro.25@mail.ru>
2024-05-30 19:34:53 +0300

    fix: static

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,25 @@ import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import mkcertPlugin from 'vite-plugin-mkcert'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     mkcertPlugin(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './src/assets/json/*',
+          dest: 'assets/json',
+        },
+        {
+          src: './src/assets/pic/*',
+          dest: 'assets/pic',
+        },
+      ],
+    }),
   ],
   base: '/tages-test/',
   resolve: {
